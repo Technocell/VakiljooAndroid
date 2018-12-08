ca611289acb95c9aa89934c180cd2c7dcaa554e2
shaghayeghamiri <shaghayeghamiricomputer@yahoo.com>
2018-12-08 16:04:43 +0330

    4.4

---
+++ app/src/main/java/ir/technocell/vakiljoo/VakilInformationActivity.java
@@ -0,0 +1,13 @@
+package ir.technocell.vakiljoo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VakilInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vakil_information);
+    }
+}
